e8a83be59c96f4a78147611078e187ce86ac0374
jakubdrewniak <drewniak.jakub@gmail.com>
2021-05-30 14:58:04 +0200

    background script added. Initial settings. Functionality testing

--- popup.js
+++ popup.js
@@ -1,4 +1,18 @@
 var despamifyButton = document.getElementById('despamify');
-despamifyButton.addEventListener('click', function () {
-    alert('Magic!')
+despamifyButton.addEventListener('click', async () => {
+    despamifyButton.style.backgroundColor = 'green'
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColor,
+    });
 }, false);
+
+function setPageBackgroundColor() {
+    chrome.storage.sync.get('autoFiltering', ({autoFiltering}) => {
+        if (autoFiltering) {
+            document.body.style.backgroundColor = "red";
+        }
+    })
+}
